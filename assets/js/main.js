a1bc56da8a39613964db01b48f37def373c58993
Patrício Rios <111129849+patricioor@users.noreply.github.com>
2022-11-30 13:31:23 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,15 @@
-const form = document.querySelector('#form')
+const form = document.querySelector('#form');
 form.addEventListener('submit', (e) => {e.preventDefault();})
+const getNumber = document.querySelector('#number');
+const styleBody = getComputedStyle(document.body);
+const colorPause = 'red';
+const colorP = 'black'
 
 let chronometer;
 
 let hour = 0;
 let minute = 0;
 let second = 0;
-let millisecond = 0;
 
 document.formTag.start.onclick = () => start();
 document.formTag.pause.onclick = () => pause();
@@ -14,29 +17,28 @@ document.formTag.reset.onclick = () => reset();
 
 function start() {
   pause();
-  chronometer = setInterval(() =>{timer(); }, 10)
+  getNumber.style.color = colorP;
+  chronometer = setInterval(() =>{timer(); }, 1000)
 }
 
 function pause(){
+  getNumber.style.color = colorPause;  
   clearInterval(chronometer);
 }
 
 function reset() {
+  pause();
+  getNumber.style.color = colorP;
   hour = 0;
   minute = 0;
   second = 0;
-  millisecond = 0;
   document.getElementById('hour').innerText = '00';
   document.getElementById('minute').innerText = '00';
   document.getElementById('second').innerText = '00';
 }
 
 function timer() {
-  if ((millisecond += 10) === 1000) {
-    millisecond = 0;
-    second++;
-  }
-  if(second === 60) {
+  if((second++) === 60) {
     second = 0;
     minute++;;
   }
