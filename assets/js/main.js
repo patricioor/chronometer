31344afc78b8c96ab8ec8e4705a5e2fd65622c27
Patrício Rios <111129849+patricioor@users.noreply.github.com>
2022-12-01 13:43:30 -0300

    Refatoring the Project
     ◦ Todo o código do projeto foi inserido em na função "chronometer()" para sair do escopo global;
     ◦ Variáveis relacionadas a tag "form" foram removidas;
     ◦ Variáveis como "hour" e "minute" tiveram suas finalidades extintas. Antes era utilizados 3 constantes(hour, minute e second) para quantificar as horas. Essa funcionalidade foi substituidas pela função construtora "new Date". "new Date" tem a função de formatar a hora com base nas informações inseridas na variável "second"(esta que será multiplicada por 1000 para transformar ms -> s);
    ◦ Inserção da cor vermelha durante a pausa do cronômetro agora é realizada por css. Critério para adicionar e remover é feito pelo JavaScript ao remover e adicionar a classe ("stoped").

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,56 +1,36 @@
-const form = document.querySelector('#form');
-form.addEventListener('submit', (e) => {e.preventDefault();})
-const getNumber = document.querySelector('#number');
-const styleBody = getComputedStyle(document.body);
-const colorPause = 'red';
-const colorP = 'black'
-
-let chronometer;
-
-let hour = 0;
-let minute = 0;
-let second = 0;
-
-document.formTag.start.onclick = () => start();
-document.formTag.pause.onclick = () => pause();
-document.formTag.reset.onclick = () => reset();
-
-function start() {
-  pause();
-  getNumber.style.color = colorP;
-  chronometer = setInterval(() =>{timer(); }, 1000)
-}
-
-function pause(){
-  getNumber.style.color = colorPause;  
-  clearInterval(chronometer);
-}
-
-function reset() {
-  pause();
-  getNumber.style.color = colorP;
-  hour = 0;
-  minute = 0;
-  second = 0;
-  document.getElementById('hour').innerText = '00';
-  document.getElementById('minute').innerText = '00';
-  document.getElementById('second').innerText = '00';
-}
-
-function timer() {
-  if((second++) === 60) {
-    second = 0;
-    minute++;;
+function chronometer() {
+  function getHourToSecond (second) {
+    const data = new Date(second * 1000)
+    return data.toLocaleTimeString("pt-BR",{ 
+    hour12: false,
+    timeZone: 'UTC'
+    });
   }
-  if(minute === 60) {
-    minute = 0;
-    hour++;
-  }
-  document.getElementById('hour').innerText= returnData(hour);
-  document.getElementById('minute').innerText= returnData(minute);
-  document.getElementById('second').innerText= returnData(second);
-}
 
-function returnData(input) {
-  return input > 10 ? input : `0${input}`
+  const chronometer = document.querySelector('.chronometer')
+
+  let second = 0;
+  let timer;
+
+  function initChronometer() { if (second === 0) {
+    timer = setInterval(() => {second++;
+    chronometer.innerHTML=getHourToSecond(second)}, 1000) }
+  }
+
+  document.addEventListener('click', (e) => {
+    const el = e.target;
+    if (el.classList.contains("start")) {
+      initChronometer();
+      chronometer.classList.remove("stoped")
+    } else if (el.classList.contains("stop")) {
+      clearInterval(timer);
+      chronometer.classList.add("stoped");
+    } else if (el.classList.contains("reset")) {
+      clearInterval(timer); 
+      chronometer.innerHTML = "00:00:00"; 
+      second = 0; 
+      chronometer.classList.remove("stoped")
+    }
+  })
 }
+chronometer();
