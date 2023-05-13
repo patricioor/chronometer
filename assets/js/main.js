826604b3ebb915f04366317ff8ec672f054a2036
Patrício Rios <111129849+patricioor@users.noreply.github.com>
2023-05-13 12:27:25 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,44 @@
 function chronometer() {
-  function getHourToSecond (second) {
-    const data = new Date(second * 1000)
-    return data.toLocaleTimeString("pt-BR",{ 
-    hour12: false,
-    timeZone: 'UTC'
+  function getHourToSecond(second) {
+    const data = new Date(second * 1000);
+    return data.toLocaleTimeString("pt-BR", {
+      hour12: false,
+      timeZone: "UTC",
     });
   }
 
-  const chronometer = document.querySelector('.chronometer')
+  const chronometer = document.querySelector(".chronometer");
 
   let second = 0;
   let timer;
 
-  function initChronometer() { if (second >= 0) {
-    timer = setInterval(() => {second++;
-    chronometer.innerHTML=getHourToSecond(second)}, 1000) }
+  function initChronometer() {
+    timer = setInterval(() => {
+      second++;
+      chronometer.innerHTML = getHourToSecond(second);
+    }, 1000);
   }
 
-  document.addEventListener('click', (e) => {
+  document.addEventListener("click", (e) => {
     const el = e.target;
     if (el.classList.contains("start")) {
-      initChronometer();
-      chronometer.classList.remove("stoped")
+      // Verifica se o cronômetro já está em contagem
+      if (!timer) {
+        initChronometer();
+      }
+      chronometer.classList.remove("stoped");
     } else if (el.classList.contains("stop")) {
+      // Pausa a contagem do cronômetro
       clearInterval(timer);
+      timer = null;
       chronometer.classList.add("stoped");
     } else if (el.classList.contains("reset")) {
-      clearInterval(timer); 
-      chronometer.innerHTML = "00:00:00"; 
-      second = 0; 
-      chronometer.classList.remove("stoped")
+      clearInterval(timer);
+      chronometer.innerHTML = "00:00:00";
+      second = 0;
+      chronometer.classList.remove("stoped");
+      timer = null; // Reseta o timer ao clicar em reset
     }
-  })
+  });
 }
 chronometer();
